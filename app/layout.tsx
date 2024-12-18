1f77803c6e31259c8356a6ba7a56b0e60262d228
iamyogurthh <saieainkhant992003@gmail.com>
2024-12-18 10:47:41 +0630

    01-create-next-app-layout

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/header/Header'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Next Amazona V2',
+  description: 'Modern ECommerce Website',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          {children}
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+            <p>Copyright @2023 - All right reserved by Next Amazona V2</p>
+          </footer>
+        </div>
+      </body>
+    </html>
+  )
+}
