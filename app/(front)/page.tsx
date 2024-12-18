c353c4fdc1f010388622685d4f46c7487dbbe395
iamyogurthh <saieainkhant992003@gmail.com>
2024-12-18 11:44:08 +0630

    06-create-details-product-page

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,7 +1,15 @@
+import ProductItem from '@/components/products/ProductItem'
+import data from '@/lib/data'
+
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl py-2">Latest Products</h1>
+      <div className="grid gap-4 md:gird-cols-3 lg:grid-cols-4 gird-cols-1">
+        {data.products.map((product) => (
+          <ProductItem key={product.slug} product={product} />
+        ))}
+      </div>
     </>
   )
 }
